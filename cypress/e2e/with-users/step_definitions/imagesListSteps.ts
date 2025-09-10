5724d129820e2a23e079a8991b00005f62c3126b
Katarzyna Rumanowska <kasia.rumanowska@canonical.com>
2025-09-10 14:05:59 +0200

    fix: delete comments

--- cypress/e2e/with-users/step_definitions/imagesListSteps.ts
+++ cypress/e2e/with-users/step_definitions/imagesListSteps.ts
@@ -1,17 +1,14 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import { generateMAASURL } from "../../utils";
 
-// Step: user is logged in
 Given("the user is logged in", () => {
-  cy.login(); // assuming you already have a custom Cypress command for login
+  cy.login();
 });
 
-// Step: navigate to images page
 When("the user navigates to the images page", () => {
   cy.visit(generateMAASURL("/images"));
 });
 
-// Step: check heading
 Then("the main toolbar heading should be {string}", (expectedHeading) => {
   cy.get("[data-testid='main-toolbar-heading']").should(
     "contain",
